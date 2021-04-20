58d4f4468527d5d20f644e7c1e908cbebbd3dd02
jiyoung-99 <1skylar1111@gmail.com>
2021-04-20 15:31:16 +0900

    new folder

--- Text_Analytics/Tokenize_prac.py
+++ Text_Analytics/Tokenize_prac.py
@@ -1,1 +1,9 @@
 from nltk import word_tokenize, sent_tokenize
+import nltk
+nltk.download('punkt')
+
+sentence = "I am working on a program that needs to create a sentence "
+words = word_tokenize(sentence)
+print(type(words), len(words))
+print(words)
+print("!!")
